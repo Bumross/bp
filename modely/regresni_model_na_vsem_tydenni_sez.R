65b08eb64bc3e9e35e52a838278565458850e3c8
Bumross <bumr007@gmail.com>
2025-03-16 16:22:50 +0100

    tydenni sezonnost

--- modely/regresni_model_na_vsem_tydenni_sez.R
+++ modely/regresni_model_na_vsem_tydenni_sez.R
@@ -36,37 +36,199 @@ par(mfrow=c(1,1))
 
 
 
-ts_pm_log_scaled <- ts(merged_data_scaled$log_total_pm, frequency = 168)
+ts_pm_log_week <- ts(log_data_week$total_pm, frequency = 21)
 
-x_reg_scaled <- as.matrix(merged_data_scaled[, c("vehicle_count", "avg_no2", 
+
+###############################################################################
+##  škálovaná
+x_reg_scaled_week <- as.matrix(scaled_data_week[, c("vehicle_count", "avg_no2", 
                                                  "total_windSpeed", "total_pressure", 
-                                                 "total_temp", "total_hum")])
+                                                 "total_temp", "total_hum", "total_windImpact")])
 
 
-arimax_manual <- Arima(ts_pm_log_scaled, 
-                       order = c(1,1,1),         
-                       seasonal = list(order = c(2,1,0), period = 168),  
-                       xreg = x_reg_scaled,     
-                       method = "ML") 
+arimax_car_scaled_week <- auto.arima(ts_pm_log_week, 
+                              xreg = x_reg_scaled_week, 
+                              d = 1,      
+                              D = 1,      
+                              seasonal = TRUE,  
+                              stepwise = FALSE, 
+                              approximation = FALSE, 
+                              trace = TRUE) 
+# 310, 210
 
-summary(arimax_scaled_168)
+coeftest(arimax_car_scaled_week)
+# nema smysl: windspeed, pressure
+
+x_reg_scaled_week <- as.matrix(scaled_data_week[, c("vehicle_count",
+                                                    "total_temp", "total_hum", "total_windImpact")])
+
+
+arimax_car_scaled_week <- Arima(ts_pm_log_week, 
+                         order = c(3,1,0),        
+                         seasonal = list(order = c(2,1,0), period = 24),  
+                         xreg = x_reg_scaled_week,  
+                         method = "CSS-ML")  
+
+
+
+summary(arimax_car_scaled_week)
+
+
 
 par(mfrow=c(2,1))
-acf(residuals(arimax_scaled_168), main="ACF reziduí ARIMAX modelu (škálovaný, týdenní sezónnost)")
-pacf(residuals(arimax_scaled_168), main="PACF reziduí ARIMAX modelu (škálovaný, týdenní sezónnost)")
+acf(residuals(arimax_car_scaled_week), main="ACF reziduí ARIMAX modelu (škálovaný, týdenní sezónnost)")
+pacf(residuals(arimax_car_scaled_week), main="PACF reziduí ARIMAX modelu (škálovaný, týdenní sezónnost)")
 par(mfrow=c(1,1))
 
-Box.test(residuals(arimax_scaled_168), type="Ljung-Box")
+Box.test(residuals(arimax_car_scaled_week), type="Ljung-Box")
 
-qqnorm(residuals(arimax_scaled_168))
-qqline(residuals(arimax_scaled_168), col="red")
+qqnorm(residuals(arimax_car_scaled_week))
+qqline(residuals(arimax_car_scaled_week), col="red")
 
-coeftest(arimax_scaled_168)
+coeftest(arimax_car_scaled_week)
 
-residuals_arimax_scaled_168 <- residuals(arimax_scaled_168)
-lm_model_scaled_168 <- lm(residuals_arimax_scaled_168 ~ x_reg_scaled)
+residuals_arimax_scaled_week <- residuals(arimax_car_scaled_week)
+lm_model_scaled_week <- lm(residuals_arimax_scaled_week ~ x_reg_scaled_week)
+
+checkresiduals(arimax_car_scaled_week)
 
 
 
-bgtest(lm_model_scaled_168)
-bptest(lm_model_scaled_168) # nekoukat na cisla, okometrie >>>>
+
+
+###############################################################################
+## normální
+x_reg_week <- as.matrix(merged_data_week[, c("vehicle_count", "avg_no2", 
+                                                    "total_windSpeed", "total_pressure", 
+                                                    "total_temp", "total_hum", "total_windImpact")])
+
+
+arimax_car_merged_week <- auto.arima(ts_pm_log_week, 
+                              xreg = x_reg_week, 
+                              d = 1,      
+                              D = 1,      
+                              seasonal = TRUE,  
+                              stepwise = FALSE, 
+                              approximation = FALSE, 
+                              trace = TRUE) 
+# 213, 010
+
+
+arimax_car_merged_week <- Arima(ts_pm_log_week, 
+                                order = c(2,1,3),        
+                                seasonal = list(order = c(0,1,0), period = 24),  
+                                xreg = x_reg_week,  
+                                method = "CSS-ML")  
+
+
+summary(arimax_car_merged_week)
+# moc spatny proste, k nicemu, nebudu delat ani zadne dalsi testy
+
+
+
+
+
+###############################################################################
+## logaritmovaná + škálovaná
+x_reg_log_scaled_week <- as.matrix(log_scaled_data_week[, c("vehicle_count", "avg_no2", 
+                                             "total_windSpeed", "total_pressure", 
+                                             "total_temp", "total_hum", "total_windImpact")])
+
+
+arimax_car_log_scaled_week <- auto.arima(ts_pm_log_week, 
+                                     xreg = x_reg_log_scaled_week, 
+                                     d = 1,      
+                                     D = 1,      
+                                     seasonal = TRUE,  
+                                     stepwise = FALSE, 
+                                     approximation = FALSE, 
+                                     trace = TRUE) 
+# 021, 210
+arimax_car_log_scaled_week <- Arima(ts_pm_log_week, 
+                                order = c(0,2,1),        
+                                seasonal = list(order = c(2,1,0), period = 24),  
+                                xreg = x_reg_log_scaled_week,  
+                                method = "CSS-ML")  
+
+
+summary(arimax_car_log_scaled_week)
+
+# pryc: vlhkost, no2, impact
+x_reg_log_scaled_week <- as.matrix(log_scaled_data_week[, c("vehicle_count", 
+                                                            "total_windSpeed", "total_pressure", 
+                                                            "total_temp")])
+
+arimax_car_log_scaled_week <- Arima(ts_pm_log_week, 
+                                    order = c(0,2,1),        
+                                    seasonal = list(order = c(2,1,0), period = 24),  
+                                    xreg = x_reg_log_scaled_week,  
+                                    method = "CSS-ML")  
+
+
+summary(arimax_car_log_scaled_week)
+# teplota je zaporna wtf?
+
+
+par(mfrow=c(2,1))
+acf(residuals(arimax_car_log_scaled_week), main="ACF reziduí ARIMAX modelu (logaritmovaná, týdenní sezónnost)")
+pacf(residuals(arimax_car_log_scaled_week), main="PACF reziduí ARIMAX modelu (logaritmovaná, týdenní sezónnost)")
+par(mfrow=c(1,1))
+
+Box.test(residuals(arimax_car_log_scaled_week), type="Ljung-Box")
+
+qqnorm(residuals(arimax_car_log_scaled_week))
+qqline(residuals(arimax_car_log_scaled_week), col="red")
+
+coeftest(arimax_car_log_scaled_week)
+
+residuals_arimax_log_scaled_week <- residuals(arimax_car_log_scaled_week)
+lm_model_log_scaled_week <- lm(residuals_arimax_log_scaled_week ~ x_reg_log_scaled_week)
+
+checkresiduals(arimax_car_log_scaled_week)
+
+
+
+
+
+
+
+###############################################################################
+## logaritmovaná
+x_reg_log_week <- as.matrix(log_data_week[, c("vehicle_count", "avg_no2", 
+                                              "total_windSpeed", "total_pressure", 
+                                              "total_temp", "total_hum", "total_windImpact")])
+
+
+arimax_car_log_week <- auto.arima(ts_pm_log_week, 
+                                  xreg = x_reg_log_week, 
+                                  d = 1,      
+                                  D = 1,      
+                                  seasonal = TRUE,  
+                                  stepwise = FALSE, 
+                                  approximation = FALSE, 
+                                  trace = TRUE) 
+
+
+summary(arimax_car_log_week)
+
+par(mfrow=c(2,1))
+acf(residuals(arimax_car_log_week), main="ACF reziduí ARIMAX modelu (logaritmovaná, týdenní sezónnost)")
+pacf(residuals(arimax_car_log_week), main="PACF reziduí ARIMAX modelu (logaritmovaná, týdenní sezónnost)")
+par(mfrow=c(1,1))
+
+Box.test(residuals(arimax_car_log_week), type="Ljung-Box")
+
+qqnorm(residuals(arimax_car_log_week))
+qqline(residuals(arimax_car_log_week), col="red")
+
+coeftest(arimax_car_log_week)
+
+residuals_arimax_log_week <- residuals(arimax_car_log_week)
+lm_model_log_week <- lm(residuals_arimax_log_week ~ x_reg_log_week)
+
+checkresiduals(arimax_car_log_week)
+
+
+###################################
+
+# nejlepsi model - skalovane hodnoty
