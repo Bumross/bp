dff67a803845e66b0332d3a70a0ff241acd105c0
Bumross <bumr007@gmail.com>
2025-06-18 23:07:46 +0200

    denni prachove castice

---
+++ analyza_dat/_prach/prach_upravy.R
@@ -0,0 +1,14 @@
+indexy <- 4400:4700
+vetsi_nez_25 <- data$data_pm100[indexy] > 25
+
+datumy_nad_25 <- data$datum[indexy][vetsi_nez_25]
+
+print(datumy_nad_25) # od 5.7 do 8.7 kazdy den cca 15-20 hodnot
+
+data$data_pm100[indexy[vetsi_nez_25]] <- NA
+
+saveRDS(data, file = "~\\bo_data_analysis\\data\\data_procesovane\\data.rds")
+
+
+# na teto bazi jsem upravil i data_denni :-)
+
