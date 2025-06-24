dcb54bc177355ce748bd95a9d9909b3492f21b93
Bumross <bumr007@gmail.com>
2025-06-24 19:34:04 +0200

    uprava pm dat - vynechani nesmyslu

--- analyza_dat/_prach/prach_upravy.R
+++ analyza_dat/_prach/prach_upravy.R
@@ -7,8 +7,9 @@ print(datumy_nad_25) # od 5.7 do 8.7 kazdy den cca 15-20 hodnot
 
 data$data_pm100[indexy[vetsi_nez_25]] <- NA
 
-saveRDS(data, file = "~\\bo_data_analysis\\data\\data_procesovane\\data.rds")
+saveRDS(data, file = "~\\bo_data_analysis\\data\\data_procesovane\\spojena_data_upravena.rds")
 
 
 # na teto bazi jsem upravil i data_denni :-)
 
+plot(data_denni$data_pm100)
